092917fb8e9ed1045e3dd37f9c711e9d5b51a721
ralph orteza <ralphlorteza@gmail.com>
2022-10-08 03:24:42 -0700

    Add resetGame function in displayController

--- script.js
+++ script.js
@@ -20,15 +20,29 @@ const gameBoard = () => {
 /* TODO: Add grid input to put mark (x or o). */
 const displayController = (() => {
   const grid = document.querySelectorAll('.grid');
+  const reset = document.querySelector('.reset');
+
+  const resetGame = () => {
+    let board = document.getElementById('grid');
+    let fields = board.children;
+    for (let i = 0; i < fields.length; i++) {
+      let field = fields[i];
+      field.textContent = '';
+      field.disabled = false;
+    }
+  };
 
   const addSign = (e) => {
     console.log("pressed!");
     console.log(e.target.value);
+    e.target.textContent = 'x';
+    e.target.disabled = true;
   }
 
+  reset.addEventListener('click', resetGame);
   grid.forEach(field => {
     field.addEventListener('click', addSign);
   });
   
-  return {addSign};
+  return {addSign, resetGame};
 })();
